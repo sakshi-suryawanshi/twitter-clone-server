1a7868530d9c59519cfcff43966e1b8779ffc2f7
sakshi-suryawanshi1 <sakshisuryawanshi1234@gmail.com>
2024-01-22 15:03:58 +0530

    init

---
+++ src/app/index.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import cors from 'cors';
+// import { typeDefs, resolvers } from './schema';
+import bodyParser from 'body-parser';
+import { prismaClient } from '../clients/db';
+import { User } from './user';
+
+export async function initServer() {
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(cors())
+  
+
+  const graphqlServer = new ApolloServer({
+    typeDefs : `
+    ${User.types}
+
+      type Query {
+          ${User.queries}
+      }
+    `,
+
+    resolvers : {
+      Query: {
+        ...User.resolvers.queries,
+      },
+      // Mutation: {},
+    },
+    
+  });
+
+  await graphqlServer.start();
+
+  app.use('/graphql',expressMiddleware(graphqlServer));
+
+
+  return app;  
+
+}
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { initServer } from "./app";
+
+async function init() {
+  const app = await initServer();
+
+  app.listen(8000, () => console.log(`server started at 8000 port`));
+
+  
+}
+
+
+init();
+
